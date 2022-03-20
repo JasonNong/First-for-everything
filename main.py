20c517476c7c1008481c295ae393330449e80d47
JasonNong <>
2022-03-20 11:51:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+secs = 6000
+mins = secs // 60
+print(secs, "seconds is", mins, "whole minutes")
+
+# 9 months later refinement
+
+secs = int(input("Number: "))
+mins = secs // 60
+print(f"{secs} seconds is {mins} whole minutes")
